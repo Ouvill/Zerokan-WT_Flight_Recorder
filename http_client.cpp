66239a63d744f803ce99b10ee150e17d20f56cb1
Ouvill <https://twitter.com/Ouvill>
2016-02-29 02:26:48 +0900

    http通信をできるようにした

--- http_client.cpp
+++ http_client.cpp
@@ -1,1 +1,57 @@
+//参考
+
 #include "http_client.h"
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace std;
+namespace asio = boost::asio;
+namespace ip = asio::ip;
+
+
+Http_Client::Http_Client(std::string url) {
+  this->url = url;
+  try
+  {
+    asio::io_service io_service;
+
+    //TCPソケットを作成する
+    ip::tcp::socket sock(io_service);
+
+    //名前解決(ホスト名からIPアドレスに変換)する
+    ip::tcp::resolver resolver(io_service);
+    ip::tcp::resolver::query query(this->url, "http");
+
+    //ホスト情報を設定する
+    ip::tcp::endpoint endpoint(*resolver.resolve(query));
+
+    //ソケットへ接続
+    sock.connect(endpoint);
+
+    //メッセージを送信
+    asio::streambuf request;
+    ostream request_ostream(&request);
+    request_ostream << "GET /ohmi_bus/tim_dsp.asp?projCd=1&eigCd=2&teicd=3314&KaiKbn=NOW&pole=1 HTTP/1.0\r\n\r\n";
+    asio::write(sock, request);
+
+    //メッセージを受信
+    asio::streambuf buffer;
+    boost::system::error_code error;
+    asio::read(sock, buffer, asio::transfer_all(), error);
+    if ( error && error != asio::error::eof)
+    {
+      std::cout << "receive failed : " << error.message() << std::endl;
+    }
+    else
+    {
+      cout << &buffer;
+    }
+
+  }
+  catch (exception& e)
+  {
+    cout << e.what();
+  }
+
+
+}
--- http_client.h
+++ http_client.h
@@ -1,10 +1,15 @@
-
-
 #ifndef ZEROKAN_WT_FLIGHT_RECORDER_HTTP_CLIENT_H
 #define ZEROKAN_WT_FLIGHT_RECORDER_HTTP_CLIENT_H
 
+#include <string>
 
-class http_client {
+class Http_Client {
+ private:
+  std::string url;
+
+ public:
+  Http_Client(std::string url);
+  void get_data(std::string &data);
 
 };
 
