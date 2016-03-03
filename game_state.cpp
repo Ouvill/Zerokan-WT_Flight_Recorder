6ca11f43519d072ab38b89d52f124e0cc348082e
Ouvill <https://twitter.com/Ouvill>
2016-03-03 17:06:03 +0900

    game_state を httpclient から asyncclient に実装を変更

--- game_state.cpp
+++ game_state.cpp
@@ -1,5 +1,6 @@
 #include "game_state.h"
-#include "http_client.h"
+#include "async_client.h"
+#include <boost/asio>
 
 GameState::GameState() {
   state_=0;
@@ -12,11 +13,16 @@ GameState::~GameState() {
 int GameState::get() {
   std::string current_map_obj;
 
-  HttpClient http("localhost");
-  if (!(http.get_data("/map_obj.json", current_map_obj))) {
+  boost::asio::io_service io_service;
+  AsyncClient client(io_service, "localhost" , "8111", "/map_obj.json");
+  io_service.run();
+
+  if (!(client.is_complete)) {
     state_ = NotRunnningClient;
     return state_;
   };
+
+  current_map_obj = client.body();
 
   if (previous_map_obj == "" && current_map_obj == "") {
     state_ = NotGaming;
