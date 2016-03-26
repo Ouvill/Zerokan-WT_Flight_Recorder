fde9823a7f59249340431369e5f975b7145764d1
Ouvill <https://twitter.com/Ouvill>
2016-03-27 02:10:40 +0900

    コンストラクタにsetUi(this)を追加 slot や signal が機能しなかったため

--- setting_form.cpp
+++ setting_form.cpp
@@ -2,6 +2,7 @@
 #include "ini_accessor.h"
 
 SettingForm::SettingForm(QWidget *parent , IniAccessor *iniAccessor ) : QWidget(parent) , iniAccessor_(iniAccessor){
+  setupUi(this);
 
   connect(okButton, SIGNAL(clicked()), this, SLOT(okClicked()));
   connect(applyButton, SIGNAL(clicked()), this, SLOT(applyCliked()));
